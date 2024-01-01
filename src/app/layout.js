ef08786f9168a5d455514c8ba6377e787049d8ce
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2024-01-01 22:20:50 +0800

    Test tolstoy

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+
 import { Inter } from 'next/font/google'
 import './globals.css'
 
@@ -11,15 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-    <Head>
-      <script>tolstoyAppKey={'8424de39-0e47-47b1-807f-9416bcf48203'}</script>
-      <script src="https://widget.gotolstoy.com/widget/widget.js" defer></script>
-    </Head>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-    </>
-
   )
 }
