898571c5dffa7cf7e98b9adaebde749b53940be1
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2024-01-04 04:17:34 +0800

    Testing client's code

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,22 @@ import Script from 'next/script';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Script
+      {/* MY CODE  */}
+      {/* <Script
         src="https://widget.gotolstoy.com/widget/widget.js" 
         strategy="beforeInteractive"
         defer
+      /> */}
+
+      {/* Their  Code  */}
+      <Script
+      defer={true}
+      async={true}
+      id="tolstoyScript"
+      src="https://widget.gotolstoy.com/widget/widget.js"
       />
-      
+
+
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -36,12 +46,16 @@ export default function Home() {
         </div>
       </div>
 
+
+      {/* MY Code  */}
         <tolstoy-carousel
           id="0izuhb0tdg6pp"
           class="tolstoy-carousel"
           data-product-id="PRODUCT_ID">
         </tolstoy-carousel>
 
+        {/* Their code  */}
+        <tolstoy-carousel id="0izuhb0tdg6pp" className="tolstoy-carousel"></tolstoy-carousel>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
