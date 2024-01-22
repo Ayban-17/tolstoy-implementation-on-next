3ac50d252caf71c1b8d8e4e07cb0c44b612d208c
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2024-01-23 01:32:23 +0800

    stories test

--- src/app/page.js
+++ src/app/page.js
@@ -57,6 +57,12 @@ export default function Home() {
         {/* Their code  */}
         <tolstoy-carousel id="0izuhb0tdg6pp" class="tolstoy-carousel"></tolstoy-carousel>
 
+      <tolstoy-stories
+        id="es1pg6gvsjej2"
+        class="tolstoy-stories"
+        data-product-id="PRODUCT_ID">
+      </tolstoy-stories>
+    
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
